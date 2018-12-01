691f929b23e0df1ccd1c4e41c125512d453209d9
Jill Irvin <jdw268@gmail.com>
2018-12-01 16:14:07 -0500

    Update SwingDataCodingChallenge.java

--- SwingDataCodingChallenge.java
+++ SwingDataCodingChallenge.java
@@ -1,7 +1,13 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package swingdatacodingchallenge;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -15,17 +21,25 @@ public class SwingDataCodingChallenge {
     public static void main(String[] args) {
         
         //test the object's methods 
-        File testFile = new File("D:\\SwingDataCodingChallenge\\latestSwing.csv");
-        Swing myCompleteSwing = new Swing(testFile);
+        //try catch for no file
+        Swing myCompleteSwing = new Swing("D:\\SwingDataCodingChallenge\\latestSwing.csv");
         
         
-        System.out.println(myCompleteSwing.searchContinuityAboveValue("ax", 0, 86, 1, 10));       
+       System.out.println(myCompleteSwing.searchContinuityAboveValue(1, 0, 7,-.89 , 2));      
+       System.out.println(myCompleteSwing.backSearchContinuityWithinRange(1, 10, 0, -.87, -.5 , 2));   
+       System.out.println(myCompleteSwing.searchContinuityAboveValueTwoSignals(1, 2, 0, 50, 0, .5 , 10));
 
-        System.out.println(myCompleteSwing.backSearchContinuityWithinRange("ax", 10, 0, -1.5, -0.5 , 10));
-
-        System.out.println(myCompleteSwing.searchContinuityAboveValueTwoSignals("ax", "ay", 0, 50, 0, .5 , 10));
-        
-        System.out.println(myCompleteSwing.searchMultiContinuityWithinRange("ax", 0, 86, -.5, 1.5, 10));   
-    }
+/*
+myCompleteSwing.searchMultiContinuityWithinRange(1, 0, 15, -0.89, 0, 2);
+for (int i = 0 ; i< myCompleteSwing.resultIndices.size(); i++){  
+    Integer[] temp = myCompleteSwing.resultIndices.get(i);
     
+    System.out.println(temp[0]);
+    System.out.println(temp[1]);
+        
+       // System.out.println(myCompleteSwing.searchMultiContinuityWithinRange("ax", 0, 86, -.5, 1.5, 10));   
+    }
+    }
+*/
+}
 }
